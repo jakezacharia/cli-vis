f33c1f940053f2a7f8a802accddb0c1b8a05da1c
jzacharia <9064844+jzacharia@users.noreply.github.com>
2024-08-16 10:19:07 -0700

    initial setup, coreaudio dependecies and makefile configured

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <AudioUnit/AudioUnit.h>
+#include <CoreAudio/CoreAudio.h>
+
+using namespace std;
+
+// create a tap to capture system audio
+
+
+int main(int argc, char *argv[])
+{
+    cout << "cli-vis running" << endl;
+
+}
